e2f5644741caf0968d17af352835c1d784ebeace
Yael Gonzalez <123508058+yaelglz@users.noreply.github.com>
2023-04-04 01:59:51 -0600

    Adelanto carrito

--- src/js/productos2.js
+++ src/js/productos2.js
@@ -28,6 +28,17 @@ menuIcon.addEventListener("click", function () {
   menuList.classList.toggle("show");
 });
 
+let carrito = [];
+
+const carritoContenedor = document.querySelector("#cartCount");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  mostrarCarrito();
+});
+
 let productos;
 // Aquí se debe cambiar el URL del servicio en el BackEnd
 const URL_MAIN = 'https://backmaque-production.up.railway.app/maque_ceramica/productos/'; //URL a donde se hace la petición
@@ -65,9 +76,10 @@ function addItems(div_Productos) { //div_Productos es el div donde se va a agreg
                 <div class="modal-body">
                   <img src="../img/Inventario/${p.uRL_Imagen}" class="img-fluid mb-3" alt="...">
                   <p>${p.descripcion}</p>
+                  <p>${p.cantidad} disponibles</p>
                   <p class="font-weight-bold mb-0">$ ${p.precio} MXN.</p>
                   <div style="text-align: right;">
-                    <button type="button" class="btn btn-canasta add-to-cart">Agregar a la canasta</button>
+                    <button onclick= "agregarProducto(${p.id})" type="button" class="btn btn-canasta add-to-cart">Agregar a la canasta</button>
                   </div>
                 </div>
               </div>
@@ -76,11 +88,12 @@ function addItems(div_Productos) { //div_Productos es el div donde se va a agreg
         `;
       }); // foreach para agregar los productos al div del HTML
 
+
+
       Array.from(document.getElementsByClassName("add-to-cart")).forEach((btn, index) => {
         btn.addEventListener("click", () => {
           document.getElementById(`modal-${index}`).style.display = "none";
           document.getElementById(`modal-${index}`).classList.remove("show");
-          document.getElementById("cart-count").textContent = parseInt(document.getElementById("cart-count").textContent) + 1;
         });
       });
 
@@ -98,12 +111,84 @@ function addItems(div_Productos) { //div_Productos es el div donde se va a agreg
           }
         });
       });
+
+      
     });//then
   }).catch(function (err) { //si hay un error
     console.log(err); //imprime el error
   });
   console.log(document.getElementById("div_Productos")); //imprime el div donde se va a agregar los productos
 }// addItems
+
+
+function agregarProducto(id){
+
+  const existe = carrito.some((item) => item.id === id);
+  if (existe) {
+    const productos = carrito.map((item) => {
+      if (item.id === id) {
+        item.cantidad++;
+        return item;
+      } });
+    } else {
+      const item = productos.find((item) => item.id === id);
+  carrito.push(item);
+  }
+
+
+
+  
+  mostrarCarrito();
+}
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body-carrito");
+  modalBody.innerHTML = "";
+  carrito.forEach((prod) => {
+    const { id, nombre, precio, uRL_Imagen, descripcion, cantidad } = prod;
+    modalBody.innerHTML += `
+    <div class="modal-contenedor">
+      <div>
+        <img src="../img/Inventario/${uRL_Imagen}" class="img-fluid img-carrito">
+      </div>
+
+      <div>
+        <p>Producto: ${nombre}</p>
+        <p>Precio: $${precio} MXN</p>
+        <p>Cantidad: ${cantidad}</p>
+
+        <button onclick="eliminarProducto(${id})" class="btn btn-danger">Eliminar producto</button>
+      </div>
+    </div>
+    `;
+  });
+  if (carrito.length === 0) {
+    modalBody.innerHTML = `
+    <p class="text-center text-primary parrafo" style="color: black !important;">¡Aun no agregaste nada!</p>
+    `
+  } 
+  
+  carritoContenedor.textContent = carrito.length;
+
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0 );
+  
+  guardarStorage();
+}
+
+function eliminarProducto(id){
+  const itemid = id;
+  carrito = carrito.filter((item) => item.id !== itemid);
+  mostrarCarrito();
+}
+
+function guardarStorage(){
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+vaciarCarrito.addEventListener("click", () => {
+  carrito.length = [];
+  mostrarCarrito();
+});
 
 window.addEventListener("load", function () { //cuando se cargue la página
   let div = document.getElementById("div_Productos"); //div donde se va a agregar los productos
@@ -145,4 +230,3 @@ const data =
 //MetodoPUT
 //MetodoDELETE
 
-// Crear la nueva tarje
